36c694d2933f0a8d5c9a835c182336dd46bd2f1a
Arshad1071 <Arshad1071@yahoo.in>
2022-03-26 15:59:54 +0530

    commit-2

--- script.js
+++ script.js
@@ -1,59 +1,88 @@
 const apiKey = 'a5dd87d3-b62c-496d-a62f-c69e4b5560de';
+const imageDispalyElement = document.getElementById("list");
 let breedList = document.getElementById("breeds");
 let categoriesList = document.getElementById("categories");
-const listElement = document.getElementById("list");
-const paginationElement = document.getElementById("pagination");
 let formData = document.getElementById("myForm");
 
 
 
 
-//Fetching breeds list
-fetch('https://api.thecatapi.com/v1/breeds', {
-    method: 'get',
-    headers: {
-        "x-api-key": apiKey
-    },
-})
-    .then(response => response.json())
-    .then(response => {
-        response.forEach(itme => {
-            let option = document.createElement("option");
-            option.text = itme.id;
-            breedList.add(option);
-        });
-    })
-    .catch((error) => {
-        console.error('Error:', error);
-    });
+
+class catApi {
+
+    constructor(imageDispalyElement) {
+        this.imageDispalyElement;
+
+        //Fetching breeds
+        fetch('https://api.thecatapi.com/v1/breeds', {
+            method: 'get',
+            headers: {
+                "x-api-key": apiKey
+            },
+        })
+            .then(response => response.json())
+            .then(response => {
+                response.forEach(itme => {
+                    let option = document.createElement("option");
+                    option.text = itme.id;
+                    breedList.add(option);
+                });
+            })
+            .catch((error) => {
+                console.error('Error:', error);
+            });
 
 
-//Fetching categories list
-fetch('https://api.thecatapi.com/v1/categories', {
-    method: 'get',
-    headers: {
-        "x-api-key": apiKey
-    },
-})
-    .then(response => response.json())
-    .then(response => {
+        //Fetching categories
+        fetch('https://api.thecatapi.com/v1/categories', {
+            method: 'get',
+            headers: {
+                "x-api-key": apiKey
+            },
+        })
+            .then(response => response.json())
+            .then(response => {
+                response.forEach(itme => {
+                    let option = document.createElement("option");
+                    option.text = itme.name;
+                    option.value = itme.id;
+                    categoriesList.add(option);
+                });
+            })
+            .catch((error) => {
+                console.error('Error:', error);
+            });
 
-        response.forEach(itme => {
-            let option = document.createElement("option");
-            option.text = itme.name;
-            option.value = itme.id;
-            categoriesList.add(option);
-        });
-    })
-    .catch((error) => {
-        console.error('Error:', error);
-    });
+    }
+
+
+
+    DisplayList(items, wrapper, rowsPerPage, page) {
+        wrapper.innerHTML = "";
+        page--;
+        let start = rowsPerPage * page;
+        let end = start + rowsPerPage;
+        let paginatedItms = items.slice(start, end);
+        for (let i = 0; i < paginatedItms.length; i++) {
+            console.log(items[i])
+            let item = paginatedItms[i];
+            let img = document.createElement('img');
+            img.src = item;
+            wrapper.appendChild(img);
+        }
+    }
+
+}
+
+const catApiOne = new catApi(imageDispalyElement);
+
 
 
 // Event Listener to form
 formData.addEventListener("submit", (e) => {
 
     e.preventDefault();
+
     let category = formData.elements.namedItem("categories").value;
     let breed = formData.elements.namedItem("breeds").value;
     let order = formData.elements.namedItem("order").value;
@@ -68,36 +97,15 @@ formData.addEventListener("submit", (e) => {
     })
         .then(response => response.json())
         .then(response => {
-            console.log(response);
             let items = [];
             response.forEach(itme => items.push(itme.url))
-            DisplayList(items, listElement, rows, currentPage);
+            catApiOne.DisplayList(items, imageDispalyElement, 9, 1);
         })
         .catch((error) => {
             console.error('Error:', error);
         });
 });
 
-
-let currentPage = 1;
-let rows = 10;
-
-
-// Function for pagiation
-const DisplayList = (items, wrapper, rowsPerPage, page) => {
-    wrapper.innerHTML = "";
-    page--;
-    let start = rowsPerPage * page;
-    let end = start + rowsPerPage;
-    let paginatedItms = items.slice(start, end);
-    for (let i = 0; i < paginatedItms.length; i++) {
-        console.log(items[i])
-        let item = paginatedItms[i];
-        let img = document.createElement('img');
-        img.src = item;
-        wrapper.appendChild(img);
-    }
-}
 
 
 
