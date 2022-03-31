17d4332d802feab7822afd2f4bbc437bef3fd139
Arshad1071 <Arshad1071@yahoo.in>
2022-03-31 12:11:59 +0530

    commit-3

--- script.js
+++ script.js
@@ -5,23 +5,18 @@ let categoriesList = document.getElementById("categories");
 let formData = document.getElementById("myForm");
 let paginationButtons = document.getElementById("pagination");
 
-
-
-
-
 class catApi {
 
     constructor(imageDispalyElement, breed, category) {
         this.imageDispalyElement;
 
-
         //Fetching breeds
         fetch('https://api.thecatapi.com/v1/breeds', {
-            method: 'get',
-            headers: {
-                "x-api-key": apiKey
-            },
-        })
+                method: 'get',
+                headers: {
+                    "x-api-key": apiKey
+                },
+            })
             .then(response => response.json())
             .then(response => {
                 response.forEach(itme => {
@@ -37,11 +32,11 @@ class catApi {
 
         //Fetching categories
         fetch('https://api.thecatapi.com/v1/categories', {
-            method: 'get',
-            headers: {
-                "x-api-key": apiKey
-            },
-        })
+                method: 'get',
+                headers: {
+                    "x-api-key": apiKey
+                },
+            })
             .then(response => response.json())
             .then(response => {
                 response.forEach(itme => {
@@ -54,7 +49,6 @@ class catApi {
             .catch((error) => {
                 console.error('Error:', error);
             });
-
     }
 
     DisplayList(items, wrapper) {
@@ -66,8 +60,10 @@ class catApi {
         }
     }
 
-    setPagiantionButtons(currentPage) {
 
+    setPagiantionButtons(currentPage, totalPage) {
+
+        paginationButtons.innerHTML = "";
         let previusBtn = document.createElement('li');
         let spanStart = document.createElement("span");
         spanStart.className = "page-link";
@@ -76,39 +72,18 @@ class catApi {
         previusBtn.appendChild(spanStart);
         paginationButtons.appendChild(previusBtn);
 
+        let btn = document.createElement('li');
+        let aTag = document.createElement('a');
+        let currentPageSpan = document.createElement("span");
+        aTag.className = "page-link";
+        btn.className = "page-item";
+        currentPageSpan.className = "sr-only";
+        btn.appendChild(aTag);
+        aTag.appendChild(currentPageSpan);
+        aTag.innerText = currentPage;
+        aTag.style.background = "#00ff6c";
+        paginationButtons.appendChild(btn);
 
-        previusBtn.addEventListener("click", (e) => {
-            e.preventDefault();
-
-        })
-
-        console.log(pages);
-
-        for (let i = currentPage - 2; i < currentPage + 3; i++) {
-            if (i == currentPage) {
-                let btn = document.createElement('li');
-                let aTag = document.createElement('a');
-                let currentPageSpan = document.createElement("span");
-                aTag.className = "page-link";
-                btn.className = "page-item";
-                currentPageSpan.className = "sr-only";
-                btn.appendChild(aTag);
-                aTag.appendChild(currentPageSpan);
-                aTag.innerText = i;
-                aTag.style.background = "#00ff6c"
-                paginationButtons.appendChild(btn);
-
-            } else {
-                let btn = document.createElement('li');
-                let aTag = document.createElement('a');
-                aTag.className = "page-link";
-                btn.className = "page-item";
-                btn.appendChild(aTag);
-                aTag.innerText = i;
-                paginationButtons.appendChild(btn);
-            }
-
-        }
         let nextBtn = document.createElement('li');
         let spanEnd = document.createElement("span");
         spanEnd.className = "page-link";
@@ -116,76 +91,83 @@ class catApi {
         nextBtn.className = "page-item";
         nextBtn.appendChild(spanEnd);
         paginationButtons.appendChild(nextBtn);
-        console.log(pages);
+
+        nextBtn.addEventListener("click", (e) => {
+            e.preventDefault();
+            if (currentPage < totalPage) {
+                currentPage++;
+                catApiOne.getImages(currentPage);
+            } else {
+                alert("Maximum pages avilable for this selection is " + totalPage);
+            }
+
+
+        })
+
+        previusBtn.addEventListener("click", (e) => {
+            e.preventDefault();
+            if (currentPage > 1) {
+                currentPage--;
+                catApiOne.getImages(currentPage);
+            } else {
+                alert("Page count is starts from 1");
+            }
+
+        })
+    }
+
+    getImages(pageCount) {
+
+        let category = formData.elements.namedItem("categories").value;
+        let breed = formData.elements.namedItem("breeds").value;
+        let order = formData.elements.namedItem("order").value;
+        let type = formData.elements.namedItem("type").value;
+        let limit = formData.elements.namedItem('limit').value;
+
+
+        fetch(`https://api.thecatapi.com/v1/images/search?limit=${limit}&category_ids=${category}&breed=${breed}&size=full&order=${order}&mime_types=${type}&page=${pageCount}`, {
+                method: 'get',
+                headers: {
+                    "x-api-key": apiKey,
+                },
+            })
+            .then(response => response.json())
+            .then(response => {
+                let items = [];
+                response.forEach(itme => {
+                    items.push(itme.url);
+                });
+                catApiOne.DisplayList(items, imageDispalyElement);
+            })
+            .catch((error) => {
+                console.error('Error:', error);
+            });
+
+
+        fetch(`https://api.thecatapi.com/v1/images/search?limit=${limit}&category_ids=${category}&breed=${breed}&size=full&order=${order}&mime_types=${type}`, {
+                method: 'get',
+                headers: {
+                    "x-api-key": apiKey,
+                },
+            })
+            .then(response => {
+                let totalPage = parseInt(response.headers.get("pagination-count") / limit);
+                console.log("Total page", totalPage);;
+                console.log("Current page", pageCount);
+                catApiOne.setPagiantionButtons(pageCount, totalPage);
+            })
+            .catch((error) => {
+                console.error('Error:', error);
+            });
+
 
     }
 }
 
 const catApiOne = new catApi(imageDispalyElement, breedList, categoriesList);
 
-
-
-// Event Listener to form &mime_types=${type}
+// Event Listener to form
 formData.addEventListener("submit", (e) => {
-
     e.preventDefault();
-
-    let category = formData.elements.namedItem("categories").value;
-    let breed = formData.elements.namedItem("breeds").value;
-    let order = formData.elements.namedItem("order").value;
-    let type = formData.elements.namedItem("type").value;
-    let limit = formData.elements.namedItem('limit').value;
-
-
-    fetch(`https://api.thecatapi.com/v1/images/search?limit=${limit}&category_ids=${category}&breed=${breed}&size=full&order=${order}`, {
-        method: 'get',
-        headers: {
-            "x-api-key": apiKey,
-        },
-    })
-        .then(response => response.json())
-        .then(response => {
-            let items = [];
-            response.forEach(itme => {
-                items.push(itme.url);
-            });
-            catApiOne.DisplayList(items, imageDispalyElement);
-        })
-        .catch((error) => {
-            console.error('Error:', error);
-        });
-
-    fetch(`https://api.thecatapi.com/v1/images/search?limit=${limit}&category_ids=${category}&breed=${breed}&size=full&order=${order}`, {
-        method: 'get',
-        headers: {
-            "x-api-key": apiKey,
-        },
-    })
-        .then(response => {
-            pages = parseInt(response.headers.get("pagination-count") / limit);
-            catApiOne.setPagiantionButtons(5);
-        })
-        .catch((error) => {
-            console.error('Error:', error);
-        });
-
-
-
+    catApiOne.getImages(1);
 });
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
